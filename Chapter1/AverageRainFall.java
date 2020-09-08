69f1db8efd2f0c403211005fe75034c248b39056
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-08 11:18:31 -0600

    Added AverageRainFall.java

---
+++ Chapter1/AverageRainFall.java
@@ -0,0 +1,20 @@
+public class AverageRainFall{
+
+    public static void main(String[] args) {
+        double April = 12;
+        double May = 14;
+        double June = 8;
+        double Average = (April + May + June)/3;
+
+        System.out.println("Rainfall for April:\t" + April);
+        System.out.println("Rainfall for May:\t" + May);
+        System.out.println("Rainfall for June:\t" + June);
+        System.out.println("Average Rainfall:\t" + Average);
+
+
+
+
+
+    }
+
+}

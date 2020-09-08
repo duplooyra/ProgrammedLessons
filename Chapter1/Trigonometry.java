99a1f1448b59132ad264b748252d06cbaaf4333e
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-08 11:23:45 -0600

    Added Trigonometry.java

---
+++ Chapter1/Trigonometry.java
@@ -0,0 +1,19 @@
+public class Trigonometry{
+
+    public static void main(String[] args) {
+        double sin = Math.sin(0.5236);
+        double cos = Math.cos(0.5236);
+        double sum = Math.sqrt(sin) + Math.sqrt(cos);
+
+        System.out.println("sine: " + sin + " cosine: " + cos + " sum of squares: " + sum );
+        
+
+        
+
+
+
+
+
+    }
+
+}

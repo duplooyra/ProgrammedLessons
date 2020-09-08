7b24cab8f405038f9db363c15e86439ba5ad0ec7
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-08 11:07:04 -0600

    Added PayrollProgram.java and ValueOfAQuadratic.java

---
+++ Chapter1/ValueOfAQuadratic.java
@@ -0,0 +1,20 @@
+public class ValueOfAQuadratic
+{  
+  public static void main ( String[] args )  
+  {
+       
+    double X = 0.0;
+    double Result = 3*(X*X)-8*X+4;  
+    System.out.println("At X = " + X + " the value is " + Result);
+    X = 2.0;
+    Result = 3*(X*X)-8*X+4; 
+    System.out.println("At X = " + X + " the value is " + Result);
+    X = 4.0;
+    Result = 3*(X*X)-8*X+4; 
+    System.out.println("At X = " + X + " the value is " + Result);
+    
+    
+    
+    
+  }
+}

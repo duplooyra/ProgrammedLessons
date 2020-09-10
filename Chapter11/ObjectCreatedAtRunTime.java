abba59bec7f791350a1bedb56ca3691380931dd2
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-10 11:21:13 -0600

    Added ObjectCreatedAtRunTime.java

---
+++ Chapter11/ObjectCreatedAtRunTime.java
@@ -0,0 +1,17 @@
+public class ObjectCreatedAtRunTime{
+
+public static void main(String[] args) {
+    String hello = "Hello World!";
+    String hello2;
+    hello2 = "Howdy howdy howdy";
+
+    System.out.println(hello);
+
+    System.out.println(hello2);
+
+
+
+
+
+}
+}

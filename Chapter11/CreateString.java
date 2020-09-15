153c3b2dbb0499ef6c20b267610d5d9384f2b81a
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-15 11:16:13 -0600

    Added CreateString.java

---
+++ Chapter11/CreateString.java
@@ -0,0 +1,16 @@
+public  class CreateString{
+    public static void main(String[] args) {
+        String first = new String("               Hello this is a test string                      ");
+        String second = first.trim();
+
+        System.out.println(first);
+        System.out.println(second);
+
+        
+
+
+
+
+
+    }
+}

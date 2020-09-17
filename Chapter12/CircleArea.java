cf03c196d0e415fdafa863f916a1d8650c50f7ba
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-16 23:40:17 -0600

    Added CricleArea.java

---
+++ Chapter12/CircleArea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CircleArea{
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Input the radius: ");
+
+    int r = sc.nextInt();
+    double area = Math.PI * (r*r);
+
+    System.out.println("The radius is: " + r + " The area is: " + area);
+
+
+
+
+
+
+}
+
+}

9b666f0442c4c9b2b461a7995544143492d92cf2
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-17 00:18:54 -0600

    Added FurtherSubstring.java

---
+++ Chapter12/FurtherSubstring.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class FurtherSubstring
+{
+  public static void main ( String[] args )
+  {
+    
+    Scanner sc = new Scanner (System.in);
+    
+    System.out.println("Enter a string: ");
+    String str = sc.nextLine();
+
+    System.out.println("Enter beginning index: ");
+    int begin = sc.nextInt();
+
+    System.out.println("Enter ending index: ");
+    int end = sc.nextInt();
+
+    String sub = str.substring(begin, end);
+
+    System.out.println("\nOriginal String:\n" + str + "\n\nSubstring:\n" + sub);
+
+  }
+}

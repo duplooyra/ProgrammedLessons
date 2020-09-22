01b3a660190d2facdf16a33ca703bc6800f73d4f
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-09-22 10:59:04 -0600

    Added more conditions to MoreTirePressure.java to give error message for pressure out of range

--- Chapter18/MoreTirePressure.java
+++ Chapter18/MoreTirePressure.java
@@ -5,26 +5,33 @@ public class MoreTirePressure{
 
         System.out.println("Input right front pressure: ");
         double rf = sc.nextInt();
+        int check = 0;
         if (rf < 35 || rf > 45){
             System.out.println("Warning: pressure is out of range");
+            check = check + 1;
         }
         System.out.println("Input right front pressure: ");
         double lf = sc.nextInt();
         if (lf < 35 || lf > 45){
             System.out.println("Warning: pressure is out of range");
+            check = check + 1;
         }
         System.out.println("Input right front pressure: ");
         double rr = sc.nextInt();
         if (rr < 35 || rr > 45){
             System.out.println("Warning: pressure is out of range");
+            check = check + 1;
         }
         System.out.println("Input right front pressure: ");
         double lr = sc.nextInt();
         if (lr < 35 || lr > 45){
             System.out.println("Warning: pressure is out of range");
+            check = check + 1;
         }
 
-        if ((rf == lf) && (rr == lr)){
+        
+
+        if ((rf == lf) && (rr == lr) && check < 0){
             System.out.println("Inflation is OK");
         }
         else{

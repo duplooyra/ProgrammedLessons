28967d45fe1625268717e1431f4c7c0eccd705fb
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-10-02 11:07:25 -0600

    Added RandomExample.java

---
+++ Examples/RandomExample.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class RandomExample{
+    public static void main(String[] args) {
+        Random randomGenerator = new Random();
+
+        int max = 100;
+
+        int n = randomGenerator.nextInt(max);
+
+        System.out.println(n);
+
+
+
+
+
+    }
+}

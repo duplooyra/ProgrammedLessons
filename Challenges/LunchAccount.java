03d8a1027d185e4ea5ea24149ac61be4dca13dd5
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-11-13 10:45:01 -0700

    Started creating the LunchAccount class

--- Challenges/LunchAccount.java
+++ Challenges/LunchAccount.java
@@ -1,3 +1,12 @@
 public class LunchAccount{
-    
+    private double balance = 100;
+    private int purchases;
+    private static final int FIRST_100_BONUS = 20;
+    private int id = 0;
+
+    public void purchase(double price){
+        if ((balance - price) >= 0){
+            balance = balance - price;
+        }
+    }
 }
--- Challenges/LunchAccountTester.java
+++ Challenges/LunchAccountTester.java
@@ -1,3 +1,6 @@
 public class LunchAccountTester{
-    
+    public static void main(String[] args) {
+        LunchAccount test = new LunchAccount();
+        test.purchase(10); 
+    }
 }

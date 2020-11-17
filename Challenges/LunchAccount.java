6e6d7ea450f6f3a37c06d7c050dfb897f71e3bb7
Ryan Du Plooy <duplooyra@s.dcsdk12.org>
2020-11-17 08:32:19 -0700

    Worked on LunchAccount.java LunchAccountTester.java

--- Challenges/LunchAccount.java
+++ Challenges/LunchAccount.java
@@ -1,12 +1,50 @@
+import java.util.Random;
 public class LunchAccount{
-    private double balance = 100;
+    Random randomGenerator = new Random();
+    private double balance;
     private int purchases;
     private static final int FIRST_100_BONUS = 20;
-    private int id = 0;
+    private int id = randomGenerator.nextInt(999999-100000) + 100000;
+    private int first_100_count =+ 1;
+
+    public void generateEmpty(){
+        balance = 0;
+    }
+
+    public void generateWithBalance(double balance){
+        if (first_100_count <= 100){
+            this.balance = balance + FIRST_100_BONUS;
+        }
+        else{
+            this.balance = balance;
+        }
+    }
+    
 
     public void purchase(double price){
         if ((balance - price) >= 0){
             balance = balance - price;
         }
+        else{
+            System.out.println("Insufficient Funds");
+        }
+    }
+
+    public void printBalance(){
+        System.out.println("$" + balance);
+    }
+
+    public void addMoney(double money){
+        if (balance == 0 && first_100_count <= 100){
+            balance = balance + money + FIRST_100_BONUS;
+            first_100_count =+ 1;
+        }
+        else{
+            balance = balance + money;
+        }
+    }
+
+    public void printID(){
+        System.out.println(id);
     }
 }
--- Challenges/LunchAccountTester.java
+++ Challenges/LunchAccountTester.java
@@ -1,6 +1,14 @@
 public class LunchAccountTester{
     public static void main(String[] args) {
         LunchAccount test = new LunchAccount();
+        test.generateWithBalance(100);
+        test.printBalance();
         test.purchase(10); 
+        test.printBalance(); // 90.0
+        test.addMoney(20);
+        test.printBalance(); // 110.0
+        test.purchase(150); // Insufficient Funds
+        test.printBalance(); // 110.0
+        test.printID();
     }
 }
